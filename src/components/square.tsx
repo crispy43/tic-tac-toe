b25aa221c8e80a301a855131380251bed0a16d90
jin <crispy4343@gmail.com>
2025-04-24 13:13:29 +0900

    초기화 및 로직 구현

---
+++ src/components/square.tsx
@@ -0,0 +1,17 @@
+
+interface Props {
+  value: string;
+  onClick: () => void;
+}
+
+export default function Square({ value, onClick }: Props) {
+  // const handleClick = () => {
+  //   setValue("X");
+  // }
+
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
